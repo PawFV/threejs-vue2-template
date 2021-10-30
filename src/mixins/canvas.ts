7de8af20ca9aeb4ed7a433ba7cf64fca25604ecb
Paw <pregonipaw@hotmail.com>
2021-10-30 17:57:37 +0300

    minor: refactor.

--- src/mixins/canvas.ts
+++ src/mixins/canvas.ts
@@ -1,4 +1,4 @@
-import { Vue, Component } from 'vue-property-decorator'
+import { Component } from 'vue-property-decorator'
 import * as THREE from 'three'
 import Home from './root'
 
--- src/views/helpers.ts
+++ src/views/helpers.ts
@@ -1,2 +1,1 @@
 export const sizes = { width: window.innerWidth, height: window.innerHeight }
-// export const sizes = { width: 600, height: 600 }
